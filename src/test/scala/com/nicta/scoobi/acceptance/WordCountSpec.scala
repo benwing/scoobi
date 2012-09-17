9d037d1d4b600a54dd71b8ec471aadce1eb51bbd
etorreborre <etorreborre@yahoo.com>
2012-09-18 09:50:03 +1000

    fixed the optimisation of combine nodes to parDos
    Conflicts:
    	src/test/scala/com/nicta/scoobi/acceptance/WordCountSpec.scala

--- src/test/scala/com/nicta/scoobi/acceptance/WordCountSpec.scala
+++ src/test/scala/com/nicta/scoobi/acceptance/WordCountSpec.scala
@@ -8,9 +8,10 @@ class WordCountSpec extends NictaHadoop {
   "Counting words frequencies must return the frequency for each word" >> { c: SC =>
 
     val frequencies =
-      DList(repeat("hello" -> 3, "world" -> 4):_*).
+      DList(repeat("hello" -> 3, "world" -> 4, "universe" -> 2):_*).
       flatMap(_.split(" ")).map((_, 1)).
       groupByKey.
+      filter { case (word, n) => word.length < 6 }.
       combine((i: Int, j: Int) => i + j).materialize
 
     persist(c)(frequencies).toSeq.sorted must_== Seq(("hello", 3), ("world", 4))
